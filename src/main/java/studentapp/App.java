2ef5c0a6a16081277d14ddf65b3f0d0fee689e79
Matthew J <78323898+KuroSamurai00@users.noreply.github.com>
2023-02-08 15:59:04 -0800

    Add files via upload
    Added menu option to App file. minor syntax corrections

--- src/main/java/studentapp/App.java
+++ src/main/java/studentapp/App.java
@@ -11,16 +11,14 @@ public class App
     
 
     public static void main(String[] args) throws Exception {
+        Boolean exitBool = false; 
 
         ArrayList<Student> studentsList = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
-        Student studentTest = new Student("Matthew", "Johnson", 4);
-
-
         System.out.println("Welcome To student application!");
 
-        
+        //adding students to database
         System.out.println("How many Students are being added?: ");
         int numOfStudents = in.nextInt();
 
@@ -28,12 +26,37 @@ public class App
             Student student = new Student();
             studentsList.add(student);
         }
+        //Menu select
+        do{
+            Scanner input = new Scanner(System.in);
 
-        studentsList.get(0).viewStatus();
-        studentsList.get(0).addCourse(4);
+            System.out.println(":::::::::::::_______________::::::::::::");
+            System.out.println("1: View Student Status");
+            System.out.println("2: Pay Balance");
+            System.out.println("3: Add Class");
+            System.out.println("4: View Student Status");
+            System.out.println("5: View Student Status");
+            System.out.println(":::::::::::::_______________::::::::::::");
 
-        studentsList.get(0).viewStatus();
+            try {
+                int choice = in.nextInt();
 
+                switch(choice){
+                    case 1: break;
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+                    case 5: break;
+                    default: break;
+                }
+                
+            } catch (NumberFormatException e) {
+                System.out.println("Not a number!");
+            }
+
+        }while(exitBool != true);
+
+     
 
         
 
